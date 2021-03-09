22738bfa2b749fab420bb8697446d4ac2704d409
Kutuusita <ivanova.fine@gmail.com>
2021-03-09 17:01:41 +0300

    app build

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const autoprefixer = require('autoprefixer');
 const precss = require('precss');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   // Итак,  чтобы вебпак начал свою работу, нужно указать главный (основной) файл, который будет включать в себя все другие необходимые файлы (модули).
@@ -75,10 +76,12 @@ module.exports = {
   // Например, плагин для минификации кода (во время сборки код подвергается очистке и минификации).
   // Или плагин для сборки html страницы и css кода (скрипты вставляются в html, куски css собираются в один файл).
   plugins: [
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({ filename: './style.css' }),
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
+
   ],
   // Кроме entry, мы можем указать поле, куда (в какой файл) собирать конечный результат. Это свойство задаётся с помощью поля output.
   // По умолчанию, весь результирующий код собирается в папку dist.
