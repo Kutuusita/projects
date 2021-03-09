5fb44f45fe16f98e61047b9eafe8f413213d2fc4
Kutuusita <ivanova.fine@gmail.com>
2021-03-09 16:49:23 +0300

    initaial commit

---
+++ src/js/views/form.js
@@ -0,0 +1,45 @@
+import{ getAutocompleteInstance, getDatePickerInstance } from '../plugins/materialize';
+
+class FormUI {
+    constructor(autocompleteInstance, datePickerInstance) {
+        this._form = document.forms['locationsControls'];
+        this.origin = document.getElementById('autocomplete-origin');
+        this.destination = document.getElementById('autocomplete-destination');
+        this.depart = document.getElementById('datepicker-depart');
+        this.return = document.getElementById('datepicker-return');
+        this.originAutocomplite = autocompleteInstance(this.origin);
+        this.destinationAutocomplite = autocompleteInstance(this.destination);
+        this.departDatePicker = datePickerInstance(this.depart);
+        this.returnDatePicker = datePickerInstance(this.return);
+    }
+
+    get form() {
+        return this._form;
+    }
+
+    get originValue() {
+        return this.origin.value;
+    }
+
+    get destinationValue() {
+        return this.destination.value;
+    }
+
+    get departDateValue() {
+        return this.departDatePicker.toString();
+    }
+
+    get returnDateValue() {
+        return this.returnDatePicker.toString();
+    }
+
+    setAutocompleteData(data) {
+        this.originAutocomplite.updateData(data);
+        this.destinationAutocomplite.updateData(data);
+    }
+
+}
+
+const formUI = new FormUI(getAutocompleteInstance, getDatePickerInstance);
+
+export default formUI;
